30b56488688a970c1b98f308db33f5a077244d7b
Felix Ospald <felix.ospald@gmail.com>
2020-03-15 15:53:14 +0100

    data update

--- data/build.py
+++ data/build.py
@@ -11,7 +11,7 @@ from functools import cmp_to_key
 ref_time = datetime.datetime(1970,1,1)
 
 data = {}
-global_case = {}
+global_case = {'id': 'World/', 'province': '', 'country': 'World', 'lat': 0.0, 'lng': 0.0, 'is_group': True}
 
 for k in ["Confirmed", "Recovered", "Deaths"]:
     ts_key = 'timeseries_' + k.lower()
@@ -112,6 +112,8 @@ for k in ["Confirmed", "Recovered", "Deaths"]:
                 else:
                     global_case[ts_key] = add_timeseries
 
+data[global_case["id"]] = global_case
+
 # add german province data
 for k in ["Confirmed"]:
     ts_key = 'timeseries_' + k.lower()
@@ -204,10 +206,6 @@ for key in keys:
     if timeseries_active:
         data[key]["timeseries_active"] = timeseries_active
 
-timeseries_active = comp_active(global_case)
-if timeseries_active:
-    global_case["timeseries_active"] = timeseries_active
-
 # calculate distance to earest neighbour
 
 min_date = 10000000000000
@@ -243,18 +241,57 @@ for k in data.keys():
                 data[k][ts_key][date] = last_val
 
 
-def cmpfunc(x, y):
+def cmp_by_name(x, y):
+    if x["country"] == global_case["country"]:
+        return -1
+    if y["country"] == global_case["country"]:
+        return 1
     if x["country"] == y["country"]:
         return 1 if x["province"] > y["province"] else -1
     return 1 if x["country"] > y["country"] else -1
+data_by_name = data.values()
+data_by_name.sort(key=cmp_to_key(cmp_by_name))
+keys_by_name = [c['id'] for c in data_by_name]
 
-sdata = data.values()
-sdata.sort(key=cmp_to_key(cmpfunc))
+def cmp_by_confirmed(x, y):
+    if not "timeseries_confirmed" in x or not "timeseries_confirmed" in y:
+        return -1
+    return y["timeseries_confirmed"][max_date] - x["timeseries_confirmed"][max_date]
+data_by_confirmed = data.values()
+data_by_confirmed.sort(key=cmp_to_key(cmp_by_confirmed))
+keys_by_confirmed = [c['id'] for c in data_by_confirmed]
+
+def cmp_by_deaths(x, y):
+    if not "timeseries_deaths" in x or not "timeseries_deaths" in y:
+        return -1
+    return y["timeseries_deaths"][max_date] - x["timeseries_deaths"][max_date]
+data_by_deaths = data.values()
+data_by_deaths.sort(key=cmp_to_key(cmp_by_deaths))
+keys_by_deaths = [c['id'] for c in data_by_deaths]
+
+def cmp_by_recovered(x, y):
+    if not "timeseries_recovered" in x or not "timeseries_recovered" in y:
+        return -1
+    return y["timeseries_recovered"][max_date] - x["timeseries_recovered"][max_date]
+data_by_recovered = data.values()
+data_by_recovered.sort(key=cmp_to_key(cmp_by_recovered))
+keys_by_recovered = [c['id'] for c in data_by_recovered]
+
+def cmp_by_active(x, y):
+    if not "timeseries_active" in x or not "timeseries_active" in y:
+        return -1
+    return y["timeseries_active"][max_date] - x["timeseries_active"][max_date]
+data_by_active = data.values()
+data_by_active.sort(key=cmp_to_key(cmp_by_active))
+keys_by_active = [c['id'] for c in data_by_active]
 
 export_data = {
         'cases': data,
-        'global_case': global_case,
-        'sorted_cases_keys': [c['id'] for c in sdata],
+        'keys_by_name': keys_by_name,
+        'keys_by_confirmed': keys_by_confirmed,
+        'keys_by_recovered': keys_by_recovered,
+        'keys_by_active': keys_by_active,
+        'keys_by_deaths': keys_by_deaths,
         'min_date': min_date,
         'max_date': max_date
 }
